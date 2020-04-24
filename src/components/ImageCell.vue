b38dea8ef48e0e34a363be80d0656a8364bd03d2
Kirill Zaidulin <zaidulinkirill@gmail.com>
2020-04-24 19:10:46 +0300

    Fix

--- src/components/ImageCell.vue
+++ src/components/ImageCell.vue
@@ -4,19 +4,39 @@ import { VImg } from 'vuetify/lib/components';
 export default {
   functional: true,
   props: {
-    width: 48,
-    value: Object,
-    resolve: x => x.url,
+    width: {
+      type: Number,
+      default: 40
+    },
+    value: {
+      type: Object,
+      required: true
+    },
+    resolve: {
+      type: Function,
+      default: x => x.url,
+    }
   },
   render(createElement, context) {
-    const { value } = context.props
+    const { value, width, resolve } = context.props
 
-    return createElement(VImg, {
+    return createElement('a', {
+      attrs: {
+        href: resolve(value),
+        target: '_blank',
+      }
+    }, [createElement(VImg, {
       props: {
         width,
+        aspectRatio: 1,
         src: resolve(value)
       },
-    });
+      style: 'border: thin solid gray;cursor:zoom-in;',
+      class: {
+        'ma-2': true,
+        'v-sheet': true
+      }
+    })]);
   },
 };
 </script>
--- src/components/ImagesCell.vue
+++ src/components/ImagesCell.vue
@@ -4,19 +4,41 @@ import { VImg } from 'vuetify/lib/components';
 export default {
   functional: true,
   props: {
-    width: 48,
-    value: [Object],
-    resolve: x => x.url,
+    width: {
+      type: Number,
+      default: 40
+    },
+    value: {
+      type: [Object],
+      required: true
+    },
+    resolve: {
+      type: Function,
+      default: x => x.url,
+    }
   },
   render(createElement, context) {
-    const { value } = context.props
+    const { value, width, resolve } = context.props
 
-    return createElement(VImg, {
-      props: {
-        width,
-        src: resolve(value)
-      },
-    });
+    return createElement('div', {}, value.map(img => {
+      return createElement('a', {
+        attrs: {
+          href: resolve(img),
+          target: '_blank',
+        }
+      }, [createElement(VImg, {
+        props: {
+          width,
+          aspectRatio: 1,
+          src: resolve(img)
+        },
+        style: 'border: thin solid gray;cursor:zoom-in;',
+        class: {
+          'ma-2': true,
+          'v-sheet': true
+        }
+      })]);
+    }))
   },
 };
 </script>
